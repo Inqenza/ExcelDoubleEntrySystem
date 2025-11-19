310a1c3923b28e27e981ebf5af9177cea11ea8d0
Inqenza <kai.hoerning@gmail.com>
2025-11-19 08:14:19 +0100

    Auto-Updater für das Excel Double Entry System
    Automatische Update-Anwendung für das Excel Double Entry System.
    
    🔍 Prüft GitHub-Releases
    ⬇ Lädt neue Versionen der Excel-Datei herunter
    ♻ Ersetzt ältere Versionen automatisch
    🧩 Wird über ein VBA-Makro aus Excel heraus gestartet
    
    Dieses Tool wird benötigt, um die Excel-Datei aktuell zu halten, ohne manuell Dateien herunterladen zu müssen.

---
+++ ExcelDoubleEntrySystemUpdate/ExcelDoubleEntrySystemUpdate/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Octokit;
+
+public class GitHubUpdater
+{
+    private readonly string owner = "Inqenza";
+    private readonly string repo = "ExcelDoubleEntrySystem";
+    private readonly string filePath = "ExcelDoubleEntrySystem.xlsm";
+    private readonly string localPath = "AppDomain.CurrentDomain.BaseDirectory";
+
+    public async Task UpdateFileAsync()
+    {
+        var github = new GitHubClient(new ProductHeaderValue("MeinApp"));
+        var latestRelease = await github.Repository.Release.GetLatest(owner, repo);
+        var asset = latestRelease.Assets[0]; // Annahme: Wir laden den ersten Asset herunter
+        var downloadUrl = asset.BrowserDownloadUrl;
+
+        using (var client = new System.Net.Http.HttpClient())
+        {
+            var content = await client.GetByteArrayAsync(downloadUrl);
+            await File.WriteAllBytesAsync(Path.Combine(localPath, filePath), content);
+        }
+
+        Console.WriteLine("Datei wurde erfolgreich aktualisiert.");
+    }
+}
